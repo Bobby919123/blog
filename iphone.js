986bd335b5b9117d354209b572bb6f5b5bd3528c
bobby919123 <bobbyddmfka@yahoo.com>
2018-05-28 15:48:45 -0700

    not toggle

--- iphone.js
+++ iphone.js
@@ -1,4 +1,4 @@
-if (window.innerWidth && window.innerWidth <= 480) {
+if (window.innerWidth && window.innerWidth <= 2000) {
 $(document).ready(function(){
 $('#header ul').addClass('show');
 $('#header').append('<div class="leftButton" onclick="toggleMenu()">Menu</div>');});
