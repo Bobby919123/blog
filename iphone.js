8292d2367ded215754241bf8ad40b5263cbdcde0
bobby919123 <bobbyddmfka@yahoo.com>
2018-05-28 15:16:16 -0700

    testing default open drawer

--- iphone.js
+++ iphone.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
 $('#header ul').addClass('hide');
 $('#header').append('<div class="leftButton" onclick="toggleMenu()">Menu</div>');});
 function toggleMenu() {
+$('#header ul').toggleClass('show');
+$('#header .leftButton').toggleClass('pressed');
 $('#header ul').toggleClass('hide');
 $('#header .leftButton').toggleClass('pressed');
 }
