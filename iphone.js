d3790c8f6a8ae2f5e5353ace274ec4d1e419ba92
bobby919123 <bobbyddmfka@yahoo.com>
2018-05-28 16:02:29 -0700

    trying to show on default

--- iphone.js
+++ iphone.js
@@ -1,9 +1,10 @@
 if (window.innerWidth && window.innerWidth <= 2000) {
 $(document).ready(function(){
-$('#header ul').addClass('show');
-$('#header').append('<div class="leftButton" onclick="toggleMenu()">Menu</div>');});
-function toggleMenu() {
-$('#header ul').toggleClass('show');
-$('#header .leftButton').toggleClass('pressed');
-}
+    $('#header ul').addClass('show');
+    $('#header').append('<div class="leftButton" onclick="toggleMenu()">Menu</div>');
+});
+    function toggleMenu() {
+        $('#header ul').toggleClass('hide');
+        $('#header .leftButton').toggleClass('pressed');
+    }
 }
