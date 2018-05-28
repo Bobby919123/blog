fabae5073130800dd27a3063d48eb6ad2965a3e2
bobby919123 <bobbyddmfka@yahoo.com>
2018-05-28 15:19:38 -0700

    testing toggle menu by default x2

--- iphone.js
+++ iphone.js
@@ -1,11 +1,11 @@
 if (window.innerWidth && window.innerWidth <= 480) {
 $(document).ready(function(){
+$('#header ul').addClass('show');
 $('#header ul').addClass('hide');
 $('#header').append('<div class="leftButton" onclick="toggleMenu()">Menu</div>');});
 function toggleMenu() {
 $('#header ul').toggleClass('show');
 $('#header .leftButton').toggleClass('pressed');
 $('#header ul').toggleClass('hide');
-$('#header .leftButton').toggleClass('pressed');
 }
 }
